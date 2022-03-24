a7db473ede07e38dd63d635927e848c270e64cfb
Xuân Hòa <95612496+xuanhoa171@users.noreply.github.com>
2022-03-24 19:39:06 +0700

    commit

---
+++ BT_MindX/Sesion 2/src/components/Name.js
@@ -0,0 +1,11 @@
+function Name () {
+    return (
+      <div>
+        <h1>Hello, world</h1>
+        <p>
+          My Name is <strong>React</strong>
+        </p>
+      </div>
+    )
+  };
+export default Name;
